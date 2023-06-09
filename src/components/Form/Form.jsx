91b261238a64675afe526eb5454f9f804d162495
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-09 03:02:17 +0300

    Create Form.jsx

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { FormStyle } from './Form.styled';
+import { useDispatch, useSelector } from 'react-redux';
+
+export const Form = ({ onSubmit }) => {
+  // const [name, setName] = useState('');
+  // const [number, setNumber] = useState('');
+  const { name, number } = useSelector(state => state);
+  console.log(name, number);
+  const dispatch = useDispatch();
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        dispatch({ type: 'setName', payload: value });
+        break;
+      case 'number':
+        dispatch({ type: 'setNumber', payload: value });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(name, number);
+    reset();
+  };
+
+  const reset = () => {
+    dispatch({ type: 'setName', payload: '' });
+    dispatch({ type: 'setNumber', payload: '' });
+  };
+  return (
+    <FormStyle onSubmit={handleSubmit}>
+      <label>
+        Name
+        <br />
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleChange}
+          value={name}
+        />
+      </label>
+      <br />
+      <label>
+        Number <br />
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleChange}
+          value={number}
+        />
+      </label>
+      <br />
+      <button type="submit">Add contact</button>
+    </FormStyle>
+  );
+};
