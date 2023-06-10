d32ef23a63784c55979e0846ddc5440d669913ed
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 02:59:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,41 @@
-import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
+import { useEffect } from 'react';
 import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  changeFilterAction,
+  deleteContactAction,
+  setContactsAction,
+} from 'store/actions';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(localStorage.getItem('contacts'));
-  // });
-  // const [filter, setFilter] = useState('');
-  const { contacts, filter } = useSelector(state => state);
-  console.log(contacts);
+  const { contacts, filter } = useSelector(state => console.log(state.contacts));
+  console.log(filter);
   const dispatch = useDispatch();
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const addContact = (name, number) => {
-    const duplicate = contacts.find(contact => contact.name === name);
+    const duplicate = contacts.find(
+      contact => contact.name === name
+    );
+console.log(duplicate);
     if (duplicate) {
       alert('Error!');
       return;
     }
-    // setContacts(prev => [...prev, { id: nanoid(), name, number }]);
-    dispatch({
-      type: 'setContacts',
-      // ТУТ ПОМИЛКА!!!
-      payload: prev => [...prev, { id: nanoid(), name, number }],
-    });
+    dispatch(setContactsAction(contacts, name, number));
   };
 
   const changeFilter = e => {
-    // setFilter(e.currentTarget.value);
-    dispatch({ type: 'setFilter', payload: e.currentTarget.value });
+    dispatch(changeFilterAction(e.currentTarget.value));
   };
 
   const deleteContact = id => {
     const filterId = contacts.filter(contact => contact.id !== id);
-    // setContacts([...filterId]);
-    dispatch({
-      type: 'setContacts',
-      payload: [...filterId],
-    });
+    dispatch(deleteContactAction(filterId));
   };
   return (
     <div>
