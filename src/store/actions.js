3171631d519c85c1a4d7a211d875b582598df1ca
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 02:59:54 +0300

    Create actions.js

---
+++ src/store/actions.js
@@ -0,0 +1,26 @@
+import { nanoid } from 'nanoid';
+
+import { createAction } from '@reduxjs/toolkit';
+
+// export const setContactsAction = (contacts, name, number) => {
+//   return {
+//     type: SETCONTACTS,
+//     payload: [...contacts, { id: nanoid(), name, number }],
+//   };
+// };
+
+// export const changeFilterAction = value => {
+//   return { type: SETFILTER, payload: value };
+// };
+
+// export const deleteContactAction = filterId => {
+//   return {
+//     type: SETCONTACTS,
+//     payload: [...filterId],
+//   };
+// };
+
+export const setContactsAction = createAction('SETCONTACTS');
+export const changeFilterAction = createAction('SETFILTER');
+
+export const deleteContactAction = createAction('SETCONTACTS');
