e399d8b61e4b1bcd843361f6380518a5138815fc
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 18:13:06 +0300

    Delete actions.js

--- src/store/actions.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const setContactsAction = createAction('SETCONTACTS');
-export const changeFilterAction = createAction('SETFILTER');
-export const deleteContactAction = createAction('SETCONTACTS');
