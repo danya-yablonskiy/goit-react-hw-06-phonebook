acfd098f42ed0397305327fc14bd400b482eaa51
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 17:57:43 +0300

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,19 +1,11 @@
-import { contactsReducer } from './contactsReducer';
-import { deleteReducer } from './deleteReducer';
-import { filterReducer } from './filterReducer';
 
-// export const reducer = (state=initialState, action) => {
-//   if (action.type === 'setContacts') {
-//     return { ...state, contacts: action.payload };
-//   }
-//   if (action.type === 'setFilter') {
-//     return { ...state, filter: action.payload };
-//   }
-//   return state;
-// };
+import { contactsReducer } from './contactsSlice';
+import { deleteReducer } from './deleteReducer';
+import { filterReducer } from './filterSlice';
+
 
 export const reducer = {
   contacts: contactsReducer,
   filter: filterReducer,
-  deleteContacts: deleteReducer
+  deleteContacts: deleteReducer,
 };
