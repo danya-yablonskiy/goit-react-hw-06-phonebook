2ac210e50c215a45f78d835d58b4436ef9f2fe9f
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 19:18:25 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -7,7 +7,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'contacts',
   storage,
-  whiteList: ['contacts'],
+  whitelist: ['contacts'],
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
 
