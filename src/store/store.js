d059b9c2d14bbf7f19d92843cb7c54159ac69b46
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 19:11:51 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,9 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'contacts',
   storage,
+  whiteList: ['contacts'],
 };
+const persistedReducer = persistReducer(persistConfig, reducer);
 
-export const store = configureStore({reducer})
+export const store = configureStore({ reducer: persistedReducer });
+export const persistor = persistStore(store);
