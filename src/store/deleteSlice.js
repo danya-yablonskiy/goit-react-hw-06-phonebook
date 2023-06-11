a9e694576c559c1ca0f64848272df060603a606e
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 18:16:44 +0300

    Update deleteSlice.js

--- src/store/deleteSlice.js
+++ src/store/deleteSlice.js
@@ -1,14 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { initStateContacts } from './initStateContacts';
 
-
-
 const deleteSlice = createSlice({
   name: 'DELETECONTACTS',
   initialState: initStateContacts,
   reducers: {
     deleteContacts: (state, { payload }) => {
-      state.contacts = payload;
+      state.contacts.push(payload);
     },
   },
 });
