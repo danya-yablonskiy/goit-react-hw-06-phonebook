cc5afbfab4688720a0b42321f1e1c0b1188b9433
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 19:11:49 +0300

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,10 @@
-
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 import { deleteReducer } from './deleteSlice';
+import { combineReducers } from 'redux';
 
-
-export const reducer = {
+export const reducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
   deleteContacts: deleteReducer,
-};
+});
