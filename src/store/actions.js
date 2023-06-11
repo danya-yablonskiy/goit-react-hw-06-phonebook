860f3bbf26e01fbf0241721e0120246b9972d858
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 14:20:27 +0300

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -22,5 +22,4 @@ import { createAction } from '@reduxjs/toolkit';
 
 export const setContactsAction = createAction('SETCONTACTS');
 export const changeFilterAction = createAction('SETFILTER');
-
 export const deleteContactAction = createAction('SETCONTACTS');
