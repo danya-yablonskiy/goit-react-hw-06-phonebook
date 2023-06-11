4c1fc2281fae70c456436a49314326b9de74464c
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 18:41:33 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 // import { createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
 
-// export const store = createStore(reducer, {
-//   contacts: JSON.parse(localStorage.getItem('contacts')),
-//   filter: '',
-// });
 export const store = configureStore({reducer})
