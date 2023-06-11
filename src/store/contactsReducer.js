acecfdc2c5c10f25e823c95ca192ec3d7cd86cb1
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 15:53:55 +0300

    Update contactsReducer.js

--- src/store/contactsReducer.js
+++ src/store/contactsReducer.js
@@ -2,6 +2,7 @@ import { createReducer } from '@reduxjs/toolkit';
 import { initStateContacts } from './initStateContacts';
 
 import { setContactsAction } from './actions';
+import { nanoid } from 'nanoid';
 
 // export const contactsReducer = (state = initStateContacts, action) => {
 //   if (action.type === SETCONTACTS) {
@@ -10,8 +11,16 @@ import { setContactsAction } from './actions';
 //   return state;
 // };
 
+// export const setContactsAction = (contacts, name, number) => {
+//   return {
+//     type: SETCONTACTS,
+//     payload: [...contacts, { id: nanoid(), name, number }],
+//   };
+// };
+
 export const contactsReducer = createReducer(initStateContacts, {
-  [setContactsAction]: (state, action) => {
-    return { ...state, contacts: action.payload };
+  [setContactsAction]: (state, { payload: { name, number } }) => {
+
+    state.contacts.push({ id: nanoid(), name, number });
   },
 });
