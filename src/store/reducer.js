2427dd7ca5280808e7a382d9e097be8a943128a1
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 03:00:04 +0300

    Create reducer.js

---
+++ src/store/reducer.js
@@ -0,0 +1,17 @@
+import { contactsReducer } from './contactsReducer';
+import { filterReducer } from './filterReducer';
+
+// export const reducer = (state=initialState, action) => {
+//   if (action.type === 'setContacts') {
+//     return { ...state, contacts: action.payload };
+//   }
+//   if (action.type === 'setFilter') {
+//     return { ...state, filter: action.payload };
+//   }
+//   return state;
+// };
+
+export const reducer = {
+  contacts: contactsReducer,
+  filter: filterReducer,
+};
