dea542c37185806549be48f8573f71d5984ed230
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-11 03:00:06 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,28 +1,10 @@
-import { act } from '@testing-library/react';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { reducer } from './reducer';
 
-const reducer = (state, action) => {
-  if (action.type === 'setName') {
-    return { ...state, name: action.payload };
-  }
-  if (action.type === 'setNumber') {
-    return { ...state, number: action.payload };
-  }
-  if (action.type === 'setContacts') {
-    return { ...state, contacts: action.payload };
-  }
-  if (action.type === 'setFilter') {
-    return { ...state, filter: action.payload };
-  }
-  return state;
-};
 
-export const store = createStore(reducer, {
-  name: '',
-  number: '',
-  // ТУТ ПОМИЛКА!!!
-  contacts: () => {
-    return JSON.parse(localStorage.getItem('contacts'));
-  },
-  filter: '',
-});
+// export const store = createStore(reducer, {
+//   contacts: JSON.parse(localStorage.getItem('contacts')),
+//   filter: '',
+// });
+export const store = configureStore({reducer})
