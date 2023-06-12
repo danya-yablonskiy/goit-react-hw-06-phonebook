e0f1efbbd4a791da5fcb1ea3c6e86db5ed3983d6
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-13 01:13:06 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,18 @@
-export const Filter = ({ onChange, value }) => {
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from 'store/filterSlice';
+
+export const Filter = () => {
+  const { filter } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const changeFilter = e => {
+    dispatch(setFilter(e.currentTarget.value));
+  };
+
   return (
     <label>
       Find contact by name <br />
-      <input type="text" value={value} onChange={onChange}></input>
+      <input type="text" value={filter} onChange={changeFilter}></input>
     </label>
   );
 };
